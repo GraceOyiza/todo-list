6698f9050a2bc70dfbd7f2fdea0b0ee5b8793986
GraceOyiza <graceoyiza13@gmail.com>
2021-04-09 11:08:54 +0100

    Delete unused file

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import renderTodos from './todos';
 import renderProject from './project';
 import renderProjectContent from './projectcontent';
 import renderCreateTask from './createtask';
-import {
-    saveAndRender, createProject, createTask, render,
-  } from './helper/common';
+import renderWork from './work';
+
 
 document.querySelector('#content').appendChild(renderProject());
 document.querySelector('#content').appendChild(renderTodos());
@@ -14,30 +13,5 @@ document.querySelector('#content').appendChild(renderTodos());
 document.querySelector('#content').appendChild(renderProjectContent());
 
 document.querySelector('#content').appendChild(renderCreateTask());
+document.querySelector('#content').appendChild(renderWork());
 
-const createDefaultProject = () => {
-    const project = createProject('Default Project');
-    if (!projects.length) {
-      const todaysDate = new Date().toISOString().slice(0, 10);
-      const task = createTask(
-        'Default Task',
-        todaysDate,
-        'A brief description',
-        'Low',
-        'Default task note',
-      );
-      project.tasks.push(task);
-      projects.push(project);
-  
-      saveAndRender(projects, project.id);
-    }
-    
-  };
-
-  window.addEventListener('load', () => {
-    // displayPage();
-    createDefaultProject();
-    render();
-    createTaskBtnEventHandler();
-    projectModalEventHandler();
-  });
